5166b46821d849d0904ce4eac4c2856ea3d289aa
GordonAnsell <moving@ga1964.com>
2021-03-29 09:24:10 +0100

    Create Integer.php

---
+++ src/Validator/Integer.php
@@ -0,0 +1,40 @@
+<?php
+
+/**
+ * This file is part of the GordyAnsell GreenFedora PHP framework.
+ *
+ * (c) Gordon Ansell <contact@gordonansell.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+namespace GreenFedora\Validator;
+
+use GreenFedora\Validator\AbstractValidator;
+use GreenFedora\Validator\ValidatorInterface;
+
+/**
+ * Integer validator.
+ *
+ * @author Gordon Ansell <contact@gordonansell.com>
+ */
+
+class Integer extends AbstractValidator implements ValidatorInterface
+{
+    /**
+     * Perform the validation.
+     * 
+     * @param   mixed       $data       Data to validate.
+     * @return  bool                    True if it's valid, else false. 
+     */
+    public function validate($data) : bool
+    {
+        if (!is_integer($data)) {
+            $this->error = vsprintf("The '%s' field must be an integer.", $this->reps);
+            return false;
+        }
+        return true;
+    }
+}

25a034482a6325470453ef5a1c3ad3371b7bdbdb
GordonAnsell <moving@ga1964.com>
2021-04-03 06:12:06 +0100

    Update Router.php

--- src/Router/Router.php
+++ src/Router/Router.php
@@ -110,7 +110,7 @@ class Router implements RouterInterface, ContainerAwareInterface
                 continue;
             }
 
-            if ($route->match1($pattern)) {
+            if ($route->match($pattern)) {
                 $this->trace4(sprintf("Matched pattern '%s' against route pattern '%s', target is '%s'.", 
                     $pattern, $route->getPattern(), $route->getTarget()));
                 $matched = $route;

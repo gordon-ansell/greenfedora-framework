ffb2cc90d7808f84995ab57e542248918c0fffb5
GordonAnsell <moving@ga1964.com>
2021-04-04 12:55:27 +0100

    Prepping console stuff.

---
+++ src/Application/AbstractConsoleApplication.php
@@ -0,0 +1,82 @@
+<?php
+
+/**
+ * This file is part of the GordyAnsell GreenFedora PHP framework.
+ *
+ * (c) Gordon Ansell <contact@gordonansell.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+namespace GreenFedora\Application;
+
+use GreenFedora\Application\AbstractApplication;
+use GreenFedora\Application\ApplicationInterface;
+use GreenFedora\Application\Input\ApplicationInputInterface;
+use GreenFedora\Application\Output\ApplicationOutputInterface;
+use GreenFedora\Router\RouterInterface;
+use GreenFedora\Template\TemplateInterface;
+use GreenFedora\DependencyInjection\ContainerInterface;
+
+use GreenFedora\Application\Exception\InvalidArgumentException;
+
+/**
+ * A console application.
+ *
+ * @author Gordon Ansell <contact@gordonansell.com>
+ */
+
+abstract class AbstractConsoleApplication extends AbstractApplication implements HttpApplicationInterface
+{
+		
+	/**
+	 * Constructor.
+	 *
+	 * @param 	ContainerInterface			$container	DI container.
+	 * @param	string						$mode 		The mode we're running in: 'dev', 'test' or 'prod'.
+	 * @param	ApplicationInputInterface	$input 		Input.
+	 * @param	ApplicationOutputInterface	$output 	Output.
+	 *
+	 * @return	void
+	 */
+	public function __construct(
+		ContainerInterface $container, 
+		string $mode = 'prod', 
+		?ApplicationInputInterface $input = null, 
+		?ApplicationOutputInterface $output = null
+		)
+	{
+		parent::__construct($container, $mode, $input, $output);
+	}
+	/**
+	 * Get the router.
+	 *
+	 * @return	RouterInterface
+	 */
+	public function getRouter() : RouterInterface
+	{
+		return $this->getInstance('router');
+	}			
+
+	/**
+	 * Get the template engine.
+	 *
+	 * @return	TemplateInterface
+	 */
+	public function getTemplate() : TemplateInterface
+	{
+		return $this->getInstance('template');
+	}			
+
+	/**
+	 * Dispatch.
+	 *
+	 * @return 	void
+	 */
+	protected function dispatch()
+	{
+	}
+
+}
---
+++ src/Console/CommandLineOpts.php
@@ -0,0 +1,85 @@
+<?php
+
+/**
+ * This file is part of the GordyAnsell GreenFedora PHP framework.
+ *
+ * (c) Gordon Ansell <contact@gordonansell.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+namespace GreenFedora\Console;
+
+use GreenFedora\Console\Exception\OutOfBoundsException;
+use GreenFedora\Application\Input\ApplicationInputInterface;
+
+
+/**
+ * Command line options.
+ *
+ * @author Gordon Ansell <contact@gordonansell.com>
+ */
+
+class CommandLineOpts implements ApplicationInputInterface
+{
+		
+	/**
+	 * Constructor.
+	 *
+     * @param   string      $mode   Execution mode.
+	 * @return	void
+	 */
+	public function __construct(string $mode = 'prod')
+	{
+		$this->args = getopt($this->opts, $this->longOpts);
+		$this->processArgs($mode);
+	}
+
+    /**
+     * Post process args (possibly based on mode).
+     * 
+     * @param   string      $mode       Mode.
+     * @return  mixed
+     */
+    protected function processArgs($mode)
+    {
+    }
+
+	/**
+	 * See if we have a particular argument.
+	 *
+	 * @param 	string 		$name 		Argument name.
+	 *
+	 * @return 	bool
+	 */
+	public function hasArg(string $name) : bool
+	{
+		return array_key_exists($name, $this->args);
+	}	
+	
+	/**
+	 * Get an argument.
+	 *
+	 * For arguments that are just present but without a value (switches) we return true.
+	 * Otherwise we return the value.
+	 *
+	 * @param 	string 		$name		Argument name.
+	 * @return	mixed
+	 *
+	 * @throws 	OutOfBoundsException 	If argument not found.
+	 */
+	public function getArg(string $name)
+	{
+		if ($this->hasArg($name)) {
+			if (false === $this->args[$name]) {
+				return true;
+			} else {
+				return $this->args[$name];
+			}	
+		}
+		throw new OutOfBoundsException(sprintf("No command line argument named '%s' found", $name));
+	}	
+
+}

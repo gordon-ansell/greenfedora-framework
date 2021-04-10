07dc9d93e8b4172ab5359c9b825610217d126d77
GordonAnsell <moving@ga1964.com>
2021-04-10 12:45:15 +0100

    Update Field.php

--- src/Form/Field/Field.php
+++ src/Form/Field/Field.php
@@ -24,7 +24,7 @@ use GreenFedora\Form\Field\Exception\InvalidArgumentException;
 use GreenFedora\Filter\FilterInterface;
 use GreenFedora\Validator\ValidatorInterface;
 
-use GreenFedora\Log\InternalDebugTrait;
+use GreenFedora\Logger\InternalDebugTrait;
 
 /**
  * Form field class.

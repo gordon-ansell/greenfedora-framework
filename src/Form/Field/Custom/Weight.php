8c840e81979ff307c2b57ca98d8b7c42efb350b5
GordonAnsell <moving@ga1964.com>
2021-04-12 05:55:58 +0100

    Update Weight.php

--- src/Form/Field/Custom/Weight.php
+++ src/Form/Field/Custom/Weight.php
@@ -62,11 +62,11 @@ class Weight extends Field
         $options = array('kg' => 'kg', 'lb' => 'lb');
         $select = new Select($this->form, ['options' => $options, 'name' => $this->getName() . 'Units'], false, false);
 
-        return $select->render();
-
-        /*
         $weightPlusUnits = $this->render() . $select->render();
 
+        return $weightPlusUnits;
+
+        /*
         $span = new Html('span', ['class' => 'duowrap']);
 
         $ret = $label->render($this->ourLabel) . PHP_EOL . $span->render($weightPlusUnits) . PHP_EOL;

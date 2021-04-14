a60d6ad985843d14fad3bff21b8033b17a60b7e9
GordonAnsell <moving@ga1964.com>
2021-04-14 06:53:20 +0100

    Create NumberFormat.php

---
+++ src/Filter/NumberFormat.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * This file is part of the GordyAnsell GreenFedora PHP framework.
+ *
+ * (c) Gordon Ansell <contact@gordonansell.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+namespace GreenFedora\Filter;
+
+use GreenFedora\Filter\AbstractFilter;
+use GreenFedora\Filter\FilterInterface;
+
+/**
+ * number_format filter.
+ *
+ * @author Gordon Ansell <contact@gordonansell.com>
+ */
+
+class NumberFormat extends AbstractFilter implements FilterInterface
+{
+    /**
+     * Perform the filter.
+     * 
+     * @param   mixed       $data       Data to validate.
+     * @return  mixed                   Filtered data. 
+     */
+    public function filter($data)
+    {
+        return number_format($data, $this->options['decimals']);
+    }
+}

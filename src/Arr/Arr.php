9920d3173d2a8e58f8e48d20f44d0ea6e15373d0
GordonAnsell <moving@ga1964.com>
2021-04-17 06:30:54 +0100

    Update Arr.php

--- src/Arr/Arr.php
+++ src/Arr/Arr.php
@@ -269,6 +269,7 @@ class Arr extends \ArrayObject implements ArrInterface
 	public function sortByCol(string $col, int $spec = SORT_ASC)
 	{
 		$tmp = $this->toArray();
+		print_r($tmp);
 		$ac  = array_column($tmp, $col);
 		print_r($ac);
 		array_multisort($ac, $spec, $tmp);

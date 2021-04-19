f4f8d589ffe751e88a502ef5f8944f09dce62110
GordonAnsell <moving@ga1964.com>
2021-04-19 07:20:31 +0100

    Update Container.php

--- src/DI/Container.php
+++ src/DI/Container.php
@@ -191,6 +191,8 @@ class Container implements ContainerInterface
 				if ($args[$count] and null !== $args[$count]) {
 					$newArgs[] = $args[$count];
 				} else if (array_key_exists('arg-' . $count, $injectables)) {
+					echo "Creating by type: 'arg-" . $count . "', " . $reflection->getName() . PHP_EOL;
+					$tmp = $this->createByType($injectables['arg-' . $count]);
 					$newArgs[] = $this->createByType($injectables['arg-' . $count]);
 				} else {
 					$newArgs[] = null;

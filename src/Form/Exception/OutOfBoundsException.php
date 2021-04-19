afb1f07e5be59a473ab3a8f3c565517d21a0bea1
GordonAnsell <moving@ga1964.com>
2021-04-19 11:29:51 +0100

    Create OutOfBoundsException.php

---
+++ src/Form/Exception/OutOfBoundsException.php
@@ -0,0 +1,25 @@
+<?php
+
+/**
+ * This file is part of the GordyAnsell GreenFedora PHP framework.
+ *
+ * (c) Gordon Ansell <contact@gordonansell.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+namespace GreenFedora\Form\Exception;
+
+use GreenFedora\Form\Exception\ExceptionInterface;
+
+/**
+ * Invalid argument passed to form field.
+ *
+ * @author Gordon Ansell <contact@gordonansell.com>
+ */
+
+class OutOfBoundsException extends \OutOfBoundsException implements ExceptionInterface
+{
+}

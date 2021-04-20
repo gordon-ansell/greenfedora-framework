553990ecdb5fd51eccdc07472807645b2e4de01d
GordonAnsell <moving@ga1964.com>
2021-04-20 07:40:26 +0100

    Update ContainerMapEntryClass.php

--- src/DI/Map/ContainerMapEntryClass.php
+++ src/DI/Map/ContainerMapEntryClass.php
@@ -45,7 +45,7 @@ class ContainerMapEntryClass extends ContainerMapEntry
     public function valueMatches($match): bool
     {
         echo "Checking " . $this->data['value'] . ' against ' . $match . '<br />' . PHP_EOL;
-        if ($this->data['value'] instanceof $match) {
+        if ($match instanceof $this->data['value']) {
             echo "Yup" . '<br />' . PHP_EOL;
         } else {
             echo "Nope" . '<br />' . PHP_EOL;

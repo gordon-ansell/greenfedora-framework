22292f9d29937d6649b56cbdac9293a078c824f2
GordonAnsell <moving@ga1964.com>
2021-04-22 15:45:54 +0100

    Update HttpRequest.php

--- src/Http/HttpRequest.php
+++ src/Http/HttpRequest.php
@@ -98,7 +98,7 @@ class HttpRequest extends Request implements HttpRequestInterface
      * 
      * @return MessageInterface
      */
-    public static function fromGlobals(array $headers = array()): HttpRequestInterface
+    public static function fromGlobals(): HttpRequestInterface
     {
         $protocol = 'HTTP/1.1';
         if (array_key_exists('SERVER_PROTOCOL', $_SERVER)) {
@@ -111,7 +111,7 @@ class HttpRequest extends Request implements HttpRequestInterface
             $_COOKIE,
             $_FILES,
             $_SERVER,
-            apache_request_headers(),
+            self::extractHeaders($_SERVER),
             null);
 
         if (0 === strpos($request->headers->get('CONTENT_TYPE'), 'application/x-www-form-urlencoded')
@@ -122,6 +122,85 @@ class HttpRequest extends Request implements HttpRequestInterface
         }
 
         return $request;
+    }
+
+    /**
+     * Extract the headers.
+     * 
+     * @param   iterable    $base   Base to extract from.
+     * @return  array
+     */
+    public static function extractHeaders(iterable $base): array
+    {
+        $headers = [];
+        foreach ($base as $key => $value) {
+            if (0 === strpos($key, 'HTTP_')) {
+                $headers[substr($key, 5)] = $value;
+            } elseif (in_array($key, ['CONTENT_TYPE', 'CONTENT_LENGTH', 'CONTENT_MD5'], true)) {
+                $headers[$key] = $value;
+            }
+        }
+
+        if (isset($base['PHP_AUTH_USER'])) {
+            $headers['PHP_AUTH_USER'] = $base['PHP_AUTH_USER'];
+            $headers['PHP_AUTH_PW'] = $base['PHP_AUTH_PW'] ?? '';
+        } else {
+            /*
+             * php-cgi under Apache does not pass HTTP Basic user/pass to PHP by default
+             * For this workaround to work, add these lines to your .htaccess file:
+             * RewriteCond %{HTTP:Authorization} .+
+             * RewriteRule ^ - [E=HTTP_AUTHORIZATION:%0]
+             *
+             * A sample .htaccess file:
+             * RewriteEngine On
+             * RewriteCond %{HTTP:Authorization} .+
+             * RewriteRule ^ - [E=HTTP_AUTHORIZATION:%0]
+             * RewriteCond %{REQUEST_FILENAME} !-f
+             * RewriteRule ^(.*)$ app.php [QSA,L]
+             */
+
+            $authorizationHeader = null;
+            if (isset($base['HTTP_AUTHORIZATION'])) {
+                $authorizationHeader = $base['HTTP_AUTHORIZATION'];
+            } elseif (isset($base['REDIRECT_HTTP_AUTHORIZATION'])) {
+                $authorizationHeader = $base['REDIRECT_HTTP_AUTHORIZATION'];
+            }
+
+            if (null !== $authorizationHeader) {
+                if (0 === stripos($authorizationHeader, 'basic ')) {
+                    // Decode AUTHORIZATION header into PHP_AUTH_USER and PHP_AUTH_PW when authorization header is basic
+                    $exploded = explode(':', base64_decode(substr($authorizationHeader, 6)), 2);
+                    if (2 == count($exploded)) {
+                        [$headers['PHP_AUTH_USER'], $headers['PHP_AUTH_PW']] = $exploded;
+                    }
+                } elseif (empty($base['PHP_AUTH_DIGEST']) && (0 === stripos($authorizationHeader, 'digest '))) {
+                    // In some circumstances PHP_AUTH_DIGEST needs to be set
+                    $headers['PHP_AUTH_DIGEST'] = $authorizationHeader;
+                    $base['PHP_AUTH_DIGEST'] = $authorizationHeader;
+                } elseif (0 === stripos($authorizationHeader, 'bearer ')) {
+                    /*
+                     * XXX: Since there is no PHP_AUTH_BEARER in PHP predefined variables,
+                     *      I'll just set $headers['AUTHORIZATION'] here.
+                     *      https://php.net/reserved.variables.server
+                     */
+                    $headers['AUTHORIZATION'] = $authorizationHeader;
+                }
+            }
+        }
+
+        if (isset($headers['AUTHORIZATION'])) {
+            return $headers;
+        }
+
+        // PHP_AUTH_USER/PHP_AUTH_PW
+        if (isset($headers['PHP_AUTH_USER'])) {
+            $headers['AUTHORIZATION'] = 'Basic '.base64_encode($headers['PHP_AUTH_USER'].':'.$headers['PHP_AUTH_PW']);
+        } elseif (isset($headers['PHP_AUTH_DIGEST'])) {
+            $headers['AUTHORIZATION'] = $headers['PHP_AUTH_DIGEST'];
+        }
+
+        return $headers;
+
     }
 
     /**
@@ -164,6 +243,7 @@ class HttpRequest extends Request implements HttpRequestInterface
 
         return $this->content;
     }
+
     	
     /**
      * Get the request URI.
